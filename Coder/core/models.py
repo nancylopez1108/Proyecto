bc62a672be0f9b0401b5ea56993f96d5572d2d01
NANCY LOPEZ <NANCYLOPEZ1108@GMAIL.COM>
2025-12-10 00:03:10 -0300

    Preparacion para aplicacion core

---
+++ Coder/core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Medicos(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    especialidad = models.CharField(max_length=40)
+
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40)
+
+
+    
+class Turno(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
